ac3fcdb056282f05cc92610e7bcdd97585bdc498
Andreu Narváez <andreu.narvaez@.estudiantat.upc.edu>
2024-11-20 13:13:15 +0100

    ejercicio resuelto sin polimorfismo

--- Polimorfismo animales/Polimorfismo animales.cpp
+++ Polimorfismo animales/Polimorfismo animales.cpp
@@ -37,3 +37,16 @@ int main()
     perr1->habla();
 
 }
+
+/*
+========================================================================================================================
+  MMP""MM""YMM `7MM"""Mq.  `YMM'   `MM'    `7MMF'  `7MMF'      db      `7MM"""Mq.  `7MM"""Yb.   `7MM"""YMM  `7MM"""Mq.
+  P'   MM   `7   MM   `MM.   VMA   ,V        MM      MM       ;MM:       MM   `MM.   MM    `Yb.   MM    `7    MM   `MM.
+	   MM        MM   ,M9     VMA ,V         MM      MM      ,V^MM.      MM   ,M9    MM     `Mb   MM   d      MM   ,M9
+	   MM        MMmmdM9       VMMP          MMmmmmmmMM     ,M  `MM      MMmmdM9     MM      MM   MMmmMM      MMmmdM9
+	   MM        MM  YM.        MM           MM      MM     AbmmmqMA     MM  YM.     MM     ,MP   MM   Y  ,   MM  YM.
+	   MM        MM   `Mb.      MM           MM      MM    A'     VML    MM   `Mb.   MM    ,dP'   MM     ,M   MM   `Mb.
+	 .JMML.    .JMML. .JMM.   .JMML.       .JMML.  .JMML..AMA.   .AMMA..JMML. .JMM..JMMmmmdP'   .JMMmmmmMMM .JMML. .JMM.
+==========================================================================================================================
+*/
+
